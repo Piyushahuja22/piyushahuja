6ab3aa75c8966b55e205deae1212e796284a295d
Your Name <ahujapiyush22@gmail.com>
2020-06-12 15:56:01 +0530

    Added Google Analytics

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,23 @@
-import { Component } from '@angular/core';
+import { Component } from "@angular/core";
+import { Router, NavigationEnd } from "@angular/router";
+
+declare let gtag: Function;
 
 @Component({
-  selector: 'app-root',
-  templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  selector: "app-root",
+  templateUrl: "./app.component.html",
+  styleUrls: ["./app.component.css"],
 })
 export class AppComponent {
-  title = 'piyush';
+  title = "piyush";
+
+  constructor(public router: Router) {
+    this.router.events.subscribe((event) => {
+      if (event instanceof NavigationEnd) {
+        gtag("config", "UA-116289090-2", {
+          'page_path': event.urlAfterRedirects,
+        });
+      }
+    });
+  }
 }
