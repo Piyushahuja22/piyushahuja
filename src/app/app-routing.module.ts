47ac5345a90ad2bf6101c476f6b22a150e1f7dee
Piyush Ahuja <ahujapiyush22@gmail.com>
2020-10-27 16:25:51 +0530

    Message

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -2,6 +2,7 @@ import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 import { AboutComponent } from './about/about.component';
 import { HomeComponent } from './home/home.component';
+import { FooterComponent } from './shared/footer/footer.component';
 import { WorksComponent } from './works/works.component';
 
 const appRoutes: Routes = [
@@ -16,6 +17,10 @@ const appRoutes: Routes = [
   {
     path: 'works',
     component: WorksComponent
+  },
+  {
+    path: 'contact',
+    component: FooterComponent
   }
 ];
 
